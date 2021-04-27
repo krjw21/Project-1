d39f53a46e48e8df5372037927dd8c439aac4070
richard <richard.kimmorley8@studytafensw.edu.au>
2021-04-27 12:00:16 +1000

    Sorrento changes

--- sorrento/src/App.js
+++ sorrento/src/App.js
@@ -4,34 +4,20 @@ import {Text} from './components/Text.js';
 import {Button} from './components/Button.js';
 import {List} from './components/List.js';
 
-const items = [
-  {"name" : "banana" , "color" : "yellow"},
-  {"name" : "apple" , "color" : "green"},
-  {"name" : "tomato" , "color" : "red"}
+const NavItems = [
+  {"name" : "Home" , "link" : "/home"},
+  {"name" : "Apartment" , "link" : "/apartment"},
+  {"name" : "Pricing" , "link" : "/pricing"},
+  {"name" : "Booking" , "link" : "/booking"},
+  {"name" : "Attractions" , "link" : "/attractions"},
+  {"name" : "Contact" , "link" : "/contact"}
 ]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Text name="Hello World!" color="purple"></Text>
-        </p>
-        <Text name="Barry" color="red"/>
-        <Text name="Larry" color="yellow"/>
-        <Text name="Harry" color="orange"/>
-        <Button start={10}/>
-        <Button start={100}/>
-        <List items={items}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="website">
+      <header className="header"></header>
+      <main className="content"></main>
+      <footer className="footer"></footer>
     </div>
   );
 }
