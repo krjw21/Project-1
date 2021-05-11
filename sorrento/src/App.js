e038e7c684c62eae5da754a38364aefaba71a948
richard <richard.kimmorley8@studytafensw.edu.au>
2021-05-11 11:44:43 +1000

    Added style for home and components

--- sorrento/src/App.js
+++ sorrento/src/App.js
@@ -1,27 +1,27 @@
 import Logo from './sorrento_by_the_sea_logo.jpg'
 import './App.css';
-import {Switch,Route} from 'react-router-dom'
-import {Home} from './components/Home.js'
-import {Apartment} from './components/Apartment.js'
-import {Pricing} from './components/Pricing.js'
-import {Booking} from './components/Booking.js'
-import {Attractions} from './components/Attractions.js'
-import {Contact} from './components/Contact.js'
-import {NotFound} from './components/NotFound.js'
-import {Header} from './components/Header.js'
+import { Switch, Route } from 'react-router-dom'
+import { Home } from './components/Home.js'
+import { Apartment } from './components/Apartment.js'
+import { Pricing } from './components/Pricing.js'
+import { Booking } from './components/Booking.js'
+import { Attractions } from './components/Attractions.js'
+import { Contact } from './components/Contact.js'
+import { NotFound } from './components/NotFound.js'
+import { Header } from './components/Header.js'
 
 const NavItems = [
-  {"name" : "Home" , "link" : "/"},
-  {"name" : "Apartment" , "link" : "/apartment"},
-  {"name" : "Pricing" , "link" : "/pricing"},
-  {"name" : "Booking" , "link" : "/booking"},
-  {"name" : "Attractions" , "link" : "/attractions"},
-  {"name" : "Contact" , "link" : "/contact"}
+  { "name": "Home", "link": "/" },
+  { "name": "Apartment", "link": "/apartment" },
+  { "name": "Pricing", "link": "/pricing" },
+  { "name": "Booking", "link": "/booking" },
+  { "name": "Attractions", "link": "/attractions" },
+  { "name": "Contact", "link": "/contact" }
 ]
 function App() {
   return (
     <div className="website">
-      <Header logo = {Logo} nav={NavItems}/>
+      <Header logo={Logo} nav={NavItems} />
       <header className="header"></header>
       <main className="content">
         <Switch>
