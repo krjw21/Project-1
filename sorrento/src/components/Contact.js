5bc5b272cfd509caa361088cc7363f7c78f9dd36
richard <richard.kimmorley8@studytafensw.edu.au>
2021-05-25 11:40:32 +1000

    GitHub hosting

--- sorrento/src/components/Contact.js
+++ sorrento/src/components/Contact.js
@@ -4,22 +4,22 @@ export function Contact(props) {
     return (
         <div className="contact">
             <h2>Contact Us</h2>
-            <form 
-            id="contact-form" 
-            action="https://example.com/handler"
-/* post method hides the data in the body
-   get method shows it in the url */
-            method="post"
+            <form
+                id="contact-form"
+                action="https://example.com/handler"
+                /* post method hides the data in the body
+                   get method shows it in the url */
+                method="post"
             >
                 {/* name */}
                 <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text" placeholder="Your Full Name"/>
+                <input id="name" name="name" type="text" placeholder="Your Full Name" />
                 {/* email */}
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" placeholder="emailhere@example.com"/>
+                <input id="email" type="email" name="email" placeholder="emailhere@example.com" />
                 {/* message */}
                 <label htmlFor="message">Message</label>
-                <textarea id="message" rows="5" cols="48" placeholder="Type your message here"/>
+                <textarea id="message" rows="5" cols="48" placeholder="Type your message here" />
                 {/* <button type="reset">Clear</button> */}
                 <button type="submit">Send</button>
             </form>
